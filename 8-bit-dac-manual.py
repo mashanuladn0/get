7e1882d2c95c75c7899b3783850a6bb76e1d0526
Мария Реснянская <resnyanskayamaria@yandex.com>
2026-02-18 17:32:50 +0300

    первый файл лабы 4

---
+++ 8-bit-dac-manual.py
@@ -0,0 +1,34 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+dac_bits = [16, 20, 21, 25, 26, 17, 27, 22]
+GPIO.setup(dac_bits, GPIO.OUT)
+dynamic_range = 3.3
+
+def voltage_to_number(voltage):
+    global dynamic_range
+    if not (0.0 <= voltage <= dynamic_range):
+        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {dynamic_range:.2f} В)")
+        print("Устанавливаем 0.0 В")
+        return 0
+    return int(voltage / dynamic_range * 255)
+
+def number_to_dac(number):
+    a = [int(i) for i in bin(number)[2:].zfill(8)]
+    print(a)
+    for i in range(len(a)):
+        GPIO.output(dac_bits[i], a[i])
+
+try:
+    while True:
+        try:
+            voltage = float(input("Введите напряжение в вольтах: "))
+            number = voltage_to_number(voltage)
+            number_to_dac(number)
+
+        except ValueError:
+            print("Вы ввели не число. Попробуйте еще раз\n")
+
+finally:
+    GPIO.output(dac_bits, 0)
+    GPIO.cleanup()
